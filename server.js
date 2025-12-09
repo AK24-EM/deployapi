ecf5ad50347545c5bcbedc02f944d547d3c9669c
personal-portfolio <aayushpkamble2006@gmail.com>
2025-12-09 15:31:58 +0530

    Remove node_modules from Git tracking

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express"
+import cors from "cors"
+import CheckcityCount from "./node_modules/cities-count/index.js"
+const app = express()
+app.use(cors)
+
+let users = [{name :"Aayush" , city:"mumbai" } , {name:"Tejas" , city:"pune"}]
+
+app.get("/api/v1/users" , (re,res)=>{
+    let count = CheckcityCount(users , "mumbai")
+    console.log(count)
+    res.send(users)
+
+})
+app.listen(8000 , ()=>{
+    console.log("server up and running")
+
+})
